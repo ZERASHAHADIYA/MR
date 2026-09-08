e9243b8ddf8d4523a77c99f9583259157f54c7ff
Zera Shahadiya <zzera1153@gmail.com>
2026-09-08 22:01:43 +0530

    Prepare application for deployment

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ function extractGeminiText(response) {
 
 // Middleware
 app.use(cors({ 
-  origin: ['http://localhost:3001', 'http://localhost:3000'], 
+  origin: [process.env.FRONTEND_URL, 'http://localhost:3001', 'http://localhost:3000'].filter(Boolean),
   credentials: true 
 }));
 app.use(express.json());
